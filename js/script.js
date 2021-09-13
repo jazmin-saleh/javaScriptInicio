fb57301bf438546863abd25feed8de17f742f6fb
jazmin-saleh <jazmin.saleh@uptc.edu.co>
2021-09-13 12:53:44 -0500

    variables(var,let,const)

---
+++ js/script.js
@@ -0,0 +1,39 @@
+alert('Hola');
+const name = prompt('Cual es tu nombre?');
+
+//para manejar la consola (console)
+
+console.log('hola '+name);
+console.error('Error');
+console.warn('Advertencia');
+
+//Variables se utiliza la palabra reservada (Var)
+//se puede colocar en una variable texto luego un entero
+var producto = "Camisa jean";
+product = "Pantalon";
+product = 20;
+
+var dispodible = true,
+    deSegunda = false,
+    economico = true;
+
+//Vraibales con (let)
+let objeto = "camiseta";
+objeto = "short";
+objeto = 60;
+
+//diferntcia entre var y let:
+//esta vota un undefined
+console.log(x);
+var x = 1;
+
+// y esta un refactorError
+console.log(i);
+let i = 2;
+
+// variable const
+const compra = "tennis";
+
+//difencia entre let y const
+//es que en const se debe inicializar y no se puede cambiar 
+//y en cuanto a let no se debe inicializar y si se puede cambiar
